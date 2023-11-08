79f80b3c7272778cba75d80011a8887d0657ab84
tbossdorf <tmbossdorf@gmail.com>
2023-11-08 14:24:46 -0500

    zombies die within specific distance of attacking player

--- src/NPCs/Zombies.java
+++ src/NPCs/Zombies.java
@@ -57,20 +57,20 @@ public class Zombies extends NPC
                 hasInteracted = true;
         }
 
-        // Point playerLocation = player.getLocation();
-        // Point zombie1L = this.getLocation();
-
-        // if (player.isAttacking() && (((Math.abs(playerLocation.x - zombie1L.x) >= 26) && (Math.abs(playerLocation.x - zombie1L.x) <= 39))
-        //     || 
-        //     ((Math.abs(playerLocation.y - zombie1L.y) <= 47) && (Math.abs(playerLocation.y-zombie1L.y) >= 34)))) {
+        // if (player.overlaps(this) && player.isAttacking()) {
+                
         //         this.isHidden = true;
-        //     }
-
-        
-        // if (isAttacking.intersects(this)) {
-        //     this.setIsHidden(true);
         // }
-        // super.update();
+
+        Point playerLocation = player.getLocation();
+        Point zombie1L = this.getLocation();
+
+        if (player.isAttacking() && (((Math.abs(playerLocation.x - zombie1L.x) >= 26) && (Math.abs(playerLocation.x - zombie1L.x) <= 39))
+            || 
+            ((Math.abs(playerLocation.y - zombie1L.y) <= 47) && (Math.abs(playerLocation.y-zombie1L.y) >= 34)))) {
+                this.isHidden = true;
+            }
+
 
         directionChangeCounter--;
 
@@ -109,29 +109,29 @@ public class Zombies extends NPC
                 {
                     new FrameBuilder(spriteSheet.getSprite(0, 0))
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
                 put("STAND_RIGHT", new Frame[] 
                 {
                     new FrameBuilder(spriteSheet.getSprite(1, 0))
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
 
                 put("WALK_LEFT", new Frame[]{
                     new FrameBuilder(spriteSheet.getSprite(1, 0), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 17, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 1), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 17, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(1, 2), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
 
@@ -139,15 +139,15 @@ public class Zombies extends NPC
                 {
                     new FrameBuilder(spriteSheet.getSprite(2, 0), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(2, 1), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(2, 2), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
 
@@ -155,30 +155,30 @@ public class Zombies extends NPC
                 {
                     new FrameBuilder(spriteSheet.getSprite(3, 0), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(3, 1), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(3, 2), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
                 put("WALK_DOWN", new Frame[]
                 {
                     new FrameBuilder(spriteSheet.getSprite(0, 0), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(0, 1), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build(),
                     new FrameBuilder(spriteSheet.getSprite(0, 2), 14)
                             .withScale(2)
-                            .withBounds(0, 0, 23, 31)
+                            .withBounds(7, 13, 20, 30)
                             .build()
                 });
         }};
